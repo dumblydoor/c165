6931aa6b99b391c5004db0d763c51bd27f25e027
dumblydoor <96972278+dumblydoor@users.noreply.github.com>
2023-08-16 18:12:14 +1000

    Add files via upload

---
+++ c164.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 12 16:38:38 2023
+
+@author: aquilavijayanayagam
+"""
+
+from tkinter import*
+import random
+root=Tk()
+root.title("random list")
+root.geometry("400x400")
+root.configure(bac)
